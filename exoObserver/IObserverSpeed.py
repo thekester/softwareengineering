d669f0c0affd61257a353bc8b587644a3f97fd28
thekester <theophile.avenel@gmail.com>
2025-01-14 09:56:51 +0100

    Update workflows

---
+++ exoObserver/IObserverSpeed.py
@@ -0,0 +1,1 @@
+notifySpeed(string vitesse)
--- list_files.py
+++ list_files.py
@@ -25,14 +25,18 @@ def list_files_excluding_dirs(root_dir, excluded_dirs, excluded_files):
 
 # Répertoire de départ
 root_directory = '.'  # Changez ce chemin si nécessaire
-excluded_directories = ['node_modules', '__pycache__', '.pytest_cache', '.git']
+excluded_directories = ['node_modules', '__pycache__', '.pytest_cache', '.git', 'data']
 excluded_files = ['package-lock.json']
 
 # Obtenir les fichiers
 files = list_files_excluding_dirs(root_directory, excluded_directories, excluded_files)
 
-# Sauvegarder les résultats dans un fichier
+# Nettoyer le fichier avant d'écrire (facultatif, 'w' le fait déjà)
 output_file = "file_contents.txt"
+if os.path.exists(output_file):
+    os.remove(output_file)
+
+# Sauvegarder les résultats dans un fichier
 with open(output_file, 'w', encoding='utf-8') as outfile:
     for file_info in files:
         outfile.write(f"Nom : {file_info['name']}\n")
